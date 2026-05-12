95b4df35aed56411bd52189f2159f9eb385a9601
Cybertee00 <faithclaws0226@gmail.com>
2026-05-12 16:59:51 +0200

    format updates

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump version to invalidate old caches when shipping shell changes.
-const VERSION = 'v2';
+const VERSION = 'v3';
 const STATIC_CACHE = 'campuscompass-static-' + VERSION;
 const RUNTIME_CACHE = 'campuscompass-runtime-' + VERSION;
 
